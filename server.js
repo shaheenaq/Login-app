b03d1ca63a8bbd747268f1f2113c42aaafbd98e0
Shaheena Qureshi <shaheenaqureshi@ShaheenasMBPro.hsd1.ga.comcast.net>
2018-03-12 21:33:42 -0400

    added passport updates to server and authentication

--- server.js
+++ server.js
@@ -41,6 +41,9 @@ app.use(session({
   resave: true
 }));
 
+app.use(passport.initialize());
+app.use(passport.session());
+
 app.use(expressValidator({
   errorFormatter: function(param, msg, value){
     var namespace = param.split('.')
